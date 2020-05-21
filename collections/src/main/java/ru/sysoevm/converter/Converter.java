bafb6afd99cef41033bd9101e16552cc422885bc
Maxim Sysoev <time_amf@mail.ru>
2020-05-22 00:22:32 +0300

    Создать convert(Iterator<Iterator>)

---
+++ collections/src/main/java/ru/sysoevm/converter/Converter.java
@@ -0,0 +1,35 @@
+package ru.sysoevm.converter;
+import java.util.*;
+
+public class Converter {
+
+    public Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new Iterator<Integer>() {
+            Iterator<Integer> iterator = it.next();
+
+            @Override
+            public boolean hasNext() {
+                boolean result = false;
+                if (iterator.hasNext()) {
+                    result = iterator.hasNext();
+                } else {
+                    result = it.hasNext();
+                }
+                return result;
+            }
+
+            @Override
+            public Integer next() {
+                int value = 0;
+                if (iterator.hasNext()) {
+                    value = iterator.next();
+                } else {
+                    iterator = it.next();
+                    value = iterator.next();
+                }
+                return value;
+            }
+        };
+    }
+
+}
