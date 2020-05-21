e982ce42085778e13415bde44204249d367e7f78
Maxim Sysoev <time_amf@mail.ru>
2020-05-21 20:48:18 +0300

    Механизм возврата монет в лендинговых аппаратах

---
+++ oop/src/main/java/ru/sysoevm/automat/Automat.java
@@ -0,0 +1,5 @@
+package ru.sysoevm.automat;
+
+public interface Automat {
+    public int[] changes(int value, int price);
+}
---
+++ oop/src/test/java/ru/sysoevm/automat/TestOperation.java
@@ -0,0 +1,15 @@
+package ru.sysoevm.automat;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class TestOperation {
+    @Test
+    public void whenBuyCofe() {
+        Operation operation = new Operation();
+        int[] resultArray = operation.changes(50, 35);
+        int[] exception = {10, 5};
+        assertThat(resultArray, is(exception));
+    }
+}
