61f60adc7679a7a85abed8fbb8c2a8f010edf681
Maxim Sysoev <time_amf@mail.ru>
2020-06-20 19:54:16 +0300

    Многопоточный increment

---
+++ multithreading/src/main/java/ru/sysoevm/count/Count.java
@@ -0,0 +1,19 @@
+package ru.sysoevm.count;
+
+import javax.annotation.concurrent.GuardedBy;
+import javax.annotation.concurrent.ThreadSafe;
+
+@ThreadSafe
+public class Count {
+
+    @GuardedBy("this")
+    private int value;
+
+    public synchronized void increment() {
+        this.value++;
+    }
+
+    public synchronized int get() {
+        return this.value;
+    }
+}
---
+++ multithreading/src/main/java/ru/sysoevm/count/package-info.java
@@ -0,0 +1,5 @@
+package ru.sysoevm.count;
+
+/*
+    Реализовать многопоточный счётчик icrement.
+ */
