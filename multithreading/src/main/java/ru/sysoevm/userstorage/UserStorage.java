cf224aea63ab2b7c22ec0e647248727b30f50e3e
Maxim Sysoev <time_amf@mail.ru>
2020-06-20 20:18:51 +0300

    Потокобезопасная структура для хранения пользователей

---
+++ multithreading/src/main/java/ru/sysoevm/userstorage/UserStorage.java
@@ -0,0 +1,64 @@
+package ru.sysoevm.userstorage;
+
+import javax.annotation.concurrent.GuardedBy;
+import javax.annotation.concurrent.ThreadSafe;
+import java.util.HashMap;
+import java.util.Map;
+
+@ThreadSafe
+public class UserStorage{
+
+    @GuardedBy("this")
+    public Map<Integer, User> users = new HashMap<Integer, User>();
+
+    public synchronized boolean add(int id, User user) {
+        if (!users.containsKey(id)) {
+            users.put(id, user);
+            return true;
+        }
+        return false;
+    }
+
+    public synchronized User getUserById(int id) {
+        User user = new User();
+        for (Map.Entry<Integer, User> item : users.entrySet()) {
+            if (item.getValue().getId() == id) {
+                user = item.getValue();
+            }
+        }
+        return user;
+    }
+
+    public synchronized boolean update (User user) {
+        User newUser =  getUserById(user.getId());
+        if (newUser!=null) {
+            newUser.setAmount(user.getAmount());
+            return true;
+        }
+        return false;
+    }
+
+    public synchronized boolean delete(User user) {
+        User newUser =  getUserById(user.getId());
+        if (newUser!=null) {
+            users.remove(user.getId());
+            return true;
+        }
+        return false;
+    }
+
+    public synchronized void transfer(int fromId, int told, int amount) {
+        User userFrom = getUserById(fromId);
+        User userTold = getUserById(told);
+        int difference = userFrom.getAmount() - amount;
+        int sum = userTold.getAmount() + amount;
+        if (difference > 0) {
+            userFrom.setAmount(difference);
+            userTold.setAmount(sum);
+        } else {
+            System.out.println("Недостаточно средств на счёте");
+        }
+    }
+
+}
+
---
+++ multithreading/src/test/java/ru/sysoevm/userstorageTest/UserStorageTest.java
@@ -0,0 +1,46 @@
+package ru.sysoevm.userstorageTest;
+
+import org.junit.Test;
+import ru.sysoevm.userstorage.User;
+import ru.sysoevm.userstorage.UserStorage;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class UserStorageTest {
+
+    private class ThreadUserStorage extends Thread {
+        private final UserStorage storage;
+        private final User user;
+        private int id;
+
+        public ThreadUserStorage(UserStorage storage, int id, User user) {
+            this.storage = storage;
+            this.id = id;
+            this.user = user;
+        }
+
+        @Override
+        public void run() {
+            storage.add(id, user);
+        }
+    }
+
+    @Test
+    public void whenTransferUser() throws InterruptedException {
+        final UserStorage store = new UserStorage();
+        User user1 = new User(1, 100);
+        User user2 = new User(2, 200);
+        Thread first = new ThreadUserStorage(store, 1, user1);
+        Thread second = new ThreadUserStorage(store, 2, user2);
+        first.start();
+        second.start();
+        first.join();
+        second.join();
+        store.transfer(1, 2, 50);
+
+        assertThat(store.users.get(2).getAmount(), is(250));
+
+    }
+}
+
