e6636907df4c4b300c7bc5d856585e88f836135d
Maxim Sysoev <time_amf@mail.ru>
2020-06-22 18:56:12 +0300

    ExecuteService рассылка почты

---
+++ multithreading/src/main/java/ru/sysoevm/emailnotification/package-info.java
@@ -0,0 +1,20 @@
+package ru.sysoevm.emailnotification;
+
+/*
+Задание.
+
+1. Реализовать сервис для рассылки почты. Создайте класс EmailNotification.
+
+2. В классе будет метод emailTo(User user) - он должен через ExecutorService отправлять почту.
+
+Так же добавьте метод close() - он должен закрыть pool. То есть в классе EmailNotification должно быть поле pool,
+которые используется в emailTo и close().
+
+3. Модель User описывают поля username, email.
+
+4. Метод emailTo должен брать данные пользователя и подставлять в шаблон subject = Notification {username} to email {email} body = Add a new event to {username}
+
+5. создайте метод public void send(String suject, String body, String email) - он должен быть пустой.
+
+6. Через ExecutorService создайте задачу, которая будет создать данные для пользователя и передавать их в метод send.
+*/
